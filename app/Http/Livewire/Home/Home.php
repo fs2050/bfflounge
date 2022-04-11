12a0c66e314b9c9518aca7058b178f55ce73e601
fs2050 <fabiolfs76@hotmail.com>
2022-04-11 08:16:03 -0300

    [Hmoe] trazendo dados do post p/ front

--- app/Http/Livewire/Home/Home.php
+++ app/Http/Livewire/Home/Home.php
@@ -4,11 +4,28 @@ namespace App\Http\Livewire\Home;
 
 use Livewire\Component;
 
+use App\Client\ClientGuzzle;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Session;
+use GuzzleHttp\Exception\ClientException;
+
 class Home extends Component
 {
+
     public function render()
     {
-        return view( 'livewire.home.index' );
-    }
+        $client = new ClientGuzzle( new Client );
+
+        $response = $client->request( 'GET', 'posts' );
+
+
+        $posts = json_decode( $response->getBody()->getContents() );
+        dump($posts);
+
+        return view( 'livewire.home.index', [
+            'posts' => $posts->data
+        ]);
+     }
+
 
 } // Home
---
+++ resources/views/livewire/home/insert-posts.blade.php
@@ -0,0 +1,27 @@
+<div>
+    <div class="modal_default" data-modal="post">
+        <div class="d-flex w-100 h-100 position-relative align-items-center justify-content-center">
+            <div class="bg"></div>
+
+            <div class="container_form">
+                <div class="d-block d-lg-flex">
+                    <div class="user_photo" style="background-image:url(assets/images/users/01.png);"></div>
+                    <form>
+                        <textarea class="mb-2"></textarea>
+                        <div class="d-flex align-items-center justify-content-between">
+                            <div>
+                                <i class="lab la-youtube mr-1">y</i>
+                                <i class="las la-camera mr-1">c</i>
+                                <i class="las la-comment">c</i>
+                            </div>
+                            <div>
+                                <button class="btn-primary">Publicar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
