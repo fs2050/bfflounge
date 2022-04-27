a70ae0fed6a2416a6fcb54dea0752a05723282cc
fs2050 <fabiolfs76@hotmail.com>
2022-04-27 15:08:19 -0300

    [Publications]Implementação Modal para Update do content do post

--- resources/views/livewire/publications/index.blade.php
+++ resources/views/livewire/publications/index.blade.php
@@ -117,50 +117,45 @@
 
             </div> <!-- -->
 
-            <div class="modal fade" id="modalEditar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
 
+
+
+            <form wire:submit.prevent="editPost">
+
+                <div class="modal fade" id="modalEditar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title">Atualizar Post</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="X">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
 
-                    <div class="modal-content">
 
-                        <div class="modal-header">
 
-                            <h5 class="modal-title">
-                                Atualizar Post
-                            </h5>
+                    <div class="form-floating">
+                        <textarea class="form-control" name="contentEditPost" id="contentEditPost"  style="height: 100px">{{ $post->content }}</textarea>
+                      </div>
+                      <div class="form-floating">
+                        <img src="{{ url("http://192.168.0.105/storage/post/{$post->medias[0]->file}") }}" class="w-100" />
+                      </div>
 
-                            <button type="button" class="close" data-dismiss="modal" aria-label="X">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                    <div class="modal-footer">
+                      <button type="submit" class="btn btn-primary" wire:click.prevent="editPost('{{ $post->id }}')">
+                        Editar
+                    </button>
 
-                        </div> <!-- -->
 
-                        <div class="modal-body">
 
-                            <textarea>
-                                {{ $post->content }}
-                            </textarea>
-
-                        </div> <!-- -->
-
-                        <div class="modal-footer">
-
-                            <a href="#" class="btn btn-primary" wire:click.prevent="editPost('{{ $post->id }}')">
-                                Salvar
-                            </a>
-
-                            <a href="#" class="btn btn-secondary" data-dismiss="modal">
-                                Fechar
-                            </a>
-
-                        </div> <!-- -->
-
-                    </div> <!-- -->
-
-                </div> <!-- -->
-
-            </div> <!-- -->
+                      <a href="#" class="btn btn-secondary" data-dismiss="modal">
+                        Fechar
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
 
             @if( empty( $post->medias ) )
                 <div class="post_info">
